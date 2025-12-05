6f840662604e9d4610a2ba4c83a1cdfd8bc36333
Sripad Karne <sripadkarne@gmail.com>
2025-12-05 11:49:45 -0500

    Starter Code for skillgap and ragretrieval scripts

--- backend/Agents/ragretrieval.py
+++ backend/Agents/ragretrieval.py
@@ -1,4 +1,20 @@
 
 
 
+from langchain.agents import create_agent
+from langchain_groq import ChatGroq
+from langchain.tools import tool
 
+import os
+
+API_KEY = os.environ["GROQ_API_KEY"]
+
+
+model = ChatGroq(
+    model = "llama-3.1-8b-instant",
+    temperature=0,
+    max_tokens=None,
+ #   reasoning_format="parsed",
+    timeout=None,
+    max_retries=2,
+)
